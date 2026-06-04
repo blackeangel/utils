7c5efbc753d9c66bc61d1a3c8b2733d4ae971fca
blackeangel <blackeangel@mail.ru>
2026-06-04 12:42:47 +0000

    fix Windows: leave HAVE_SYS_IOCTL_H undefined (not =0) on Windows
    Previous fix set '#define HAVE_SYS_IOCTL_H 0' on Windows but
    libarchive uses '#ifdef HAVE_SYS_IOCTL_H' which checks PRESENCE
    of the macro, not its value. Even =0 triggers the ifdef.
    Fix: simply don't define it on Windows so #ifdef is false.
    Same pattern as HAVE_FICLONERANGE fix applied earlier.

--- src/libarchive/config.h
+++ src/libarchive/config.h
@@ -65,9 +65,7 @@
 #define HAVE_GLOB_H 1
 #define HAVE_GRP_H 1
 #define HAVE_PWD_H 1
-#ifdef _WIN32
-#  define HAVE_SYS_IOCTL_H 0
-#else
+#ifndef _WIN32
 #  define HAVE_SYS_IOCTL_H 1
 #endif
 #define HAVE_SYS_UTSNAME_H 1
